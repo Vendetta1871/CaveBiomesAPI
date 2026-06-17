e1c76afcc61105ad336c79822627680a94204ff0
CelestialD <conqueror.v.v.v.v.v@gmail.com>
2026-06-17 14:30:05 +0800

    Bump version to 1.0.0, fix MixinBootstrap dependency name in README

--- src/main/java/net/celestiald/cavebiomes/CaveBiomesMod.java
+++ src/main/java/net/celestiald/cavebiomes/CaveBiomesMod.java
@@ -12,7 +12,7 @@ public class CaveBiomesMod {
 
     public static final String MODID   = "cavebiomesapi";
     public static final String NAME    = "Cave Biomes API";
-    public static final String VERSION = "0.1.0";
+    public static final String VERSION = "1.0.0";
 
     @Mod.Instance
     public static CaveBiomesMod INSTANCE;
